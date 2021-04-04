e5a7ed60431ed3b7ac18ecbf8036e97cb48a7df1
PaulosKal <p18kall@ionio.gr>
2021-04-04 18:49:34 +0300

    Fix path for script

--- script/html-to-pdf.sh
+++ script/html-to-pdf.sh
@@ -6,4 +6,3 @@ pandoc output.tex -o CV.pdf \
 --latex-engine=xelatex \
 -V mainfont="LiberationMono-Regular.ttf" \
 -V mainfontoptions="BoldFont=LiberationMono-Bold.ttf, ItalicFont=LiberationMono-Italic.ttf, BoldItalicFont=LiberationMono-BoldItalic.ttf"
-mv CV.pdf ../
